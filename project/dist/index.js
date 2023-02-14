43fbd39a6d483683236b443cca6b5e5bd558b030
Fauzul Ashari <paujul@192.168.1.10>
2023-02-14 13:32:21 +0700

    Baru banget.

--- project/dist/index.js
+++ project/dist/index.js
@@ -1,9 +1,24 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
-console.log(btn);
-btn.addEventListener("click", () => {
-    console.log(input.value);
-});
+const form = document.getElementById("todoform");
+const list = document.getElementById("todolist");
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   // console.log(input.value);
+//   console.log("SUBMITTED!");
+// });
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = input.value;
+    const newLi = document.createElement("li");
+    newLi.append(newTodo);
+    list.append(newLi);
+    console.log("SUBMITTED!");
+}
+form.addEventListener("submit", handleSubmit);
+// btn.addEventListener("click", () => {
+//   console.log(input.value);
+// });
 //  const mystery: unknown = "Hello World"
 //  const numChars = (mystery as string).length
--- project/src/index.ts
+++ project/src/index.ts
@@ -1,10 +1,28 @@
 const btn = document.getElementById("btn")!;
 const input = document.getElementById("todoinput") as HTMLInputElement;
-console.log(btn);
+const form = document.getElementById("todoform") as HTMLFormElement;
+const list = document.getElementById("todolist") as HTMLUListElement;
 
-btn.addEventListener("click", () => {
-  console.log(input.value);
-});
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   // console.log(input.value);
+//   console.log("SUBMITTED!");
+// });
+
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo = input.value;
+  const newLi = document.createElement("li");
+  newLi.append(newTodo);
+  list.append(newLi);
+  console.log("SUBMITTED!");
+}
+
+form.addEventListener("submit", handleSubmit);
+
+// btn.addEventListener("click", () => {
+//   console.log(input.value);
+// });
 
 //  const mystery: unknown = "Hello World"
 
